51ae0e52e0b570095e58e5f0203579ac4546f7e6
Alexandr Gotsulov <agotsulov@gmail.com>
2022-10-01 14:43:56 +0600

    wip

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,21 +9,27 @@ public class PlayerController : MonoBehaviour
 
     public Rigidbody body;
 
+    public Vector3 ass;
+
     void Start()
     {
-
+        body = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        // body.AddForce(transform.forward * speed, ForceMode.VelocityChange);
         body.velocity = transform.forward * speed;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.RotateAround(transform.position, Vector3.up, 10);
+            transform.RotateAround(transform.position - ass, Vector3.up, 1);
         }
+        Debug.Log(transform.rotation.y);
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.RotateAround(transform.position, Vector3.up, -10);
+            transform.RotateAround(transform.position - ass, Vector3.up, -1);
         }
+        if (transform.rotation.y <= 0.5 && transform.rotation.y >= -0.5) { }
+        if (transform.rotation.y <= 0.5 && transform.rotation.y >= -0.5) { };
     }
 }
